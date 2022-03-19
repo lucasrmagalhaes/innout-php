88828a5274e15d45de7b47416d31569cf98ef8af
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-19 14:58:03 -0300

    Obtendo Usuários do Banco

--- public/index.php
+++ public/index.php
@@ -4,8 +4,12 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-// echo $user->getSelect();
 
-echo User::getSelect(['id' => 1], 'name, email');
-echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+print_r(User::get(['name' => 'Chaves'], 'id, name, email'));
+
+echo '<br><br>';
+
+foreach(User::get([], 'name') as $user) {
+    echo $user->name;
+    echo '<br>';
+}
