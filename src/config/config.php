0d9b642f8e3215352e5fbfbcf59ca37db734ead4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-19 13:45:41 -0300

    Classe User 01

---
+++ src/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
+
+// Pastas
+define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+
+require_once(realpath(dirname(__FILE__) . '/database.php'));
---
+++ src/models/User.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once(realpath(MODEL_PATH . '/Model.php'));
+
+class User extends Model {
+    protected static $tableName = 'users';
+    protected static $columns = [
+        'id',
+        'name',
+        'password',
+        'email',
+        'start_date',
+        'end_date',
+        'is_admin'
+    ];  
+}
