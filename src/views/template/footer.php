d20e7ceb80ff63060c784a101bc44ac628774fd1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-26 21:39:48 -0300

    Escondendo Menu Lateral

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,8 +1,10 @@
-    </body>
-
     <footer class="footer">
         <span>Desenvolvido com</span>
         <span><i class="icofont-heart text-danger mx-1"></i></span>
         <span>por Lucas Magalhães.</span>
     </footer>
+
+    <script src="assets/js/app.js"></script>
+    
+    </body>
 </html>
