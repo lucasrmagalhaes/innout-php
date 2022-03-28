c3fa2cf91333a8847dbc0885fc9e9dd2a88a7d97
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-28 20:41:14 -0300

    Funções Utilitárias de Data #01

--- src/config/config.php
+++ src/config/config.php
@@ -17,6 +17,7 @@ define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 require_once(realpath(dirname(__FILE__) . '/database.php'));
 require_once(realpath(dirname(__FILE__) . '/loader.php'));
 require_once(realpath(dirname(__FILE__) . '/session.php'));
+require_once(realpath(dirname(__FILE__) . '/date_utils.php'));
 require_once(realpath(MODEL_PATH . '/Model.php'));
 require_once(realpath(MODEL_PATH . '/User.php'));
 require_once(realpath(EXCEPTION_PATH . '/AppException.php'));
