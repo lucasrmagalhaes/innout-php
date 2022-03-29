449d2dd236601b24fa6a53f1afc0ee962aec8245
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-28 22:06:04 -0300

    Geração de Massa de Dados #02

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -1,5 +1,10 @@
 <?php
 
+loadModel('WorkingHours');
+
+Database::executeSQL('DELETE FROM working_hours');
+Database::executeSQL('DELETE FROM users WHERE id > 5');
+
 function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
     $regularDayTemplate = [
         'time1' => '08:00:00',
@@ -35,3 +40,32 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
         return $lazyDayTemplate;
     }
 }
+
+function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate) {
+    $currentDate = $initialDate;
+
+    $today = new DateTime();
+
+    $columns = ['user_id' => $userId, 'work_date' => $currentDate];
+
+    while (isBefore($currentDate, $today)) {
+        if (!isWeekend($currentDate)) {
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+
+            $columns = array_merge($columns, $template);
+
+            $workingHours = new WorkingHours($columns);
+            $workingHours->save();
+        }
+
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+
+        $columns['work_date'] = $currentDate;
+    }
+}
+
+$lastMonth = strtotime('first day of last month');
+
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
+populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70);
