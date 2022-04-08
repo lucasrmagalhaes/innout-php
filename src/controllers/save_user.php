03e676a7e2b300a4080c642efa7d743f0a59fdb4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-08 01:24:42 -0300

    Inserir Usuário

--- src/controllers/save_user.php
+++ src/controllers/save_user.php
@@ -2,34 +2,21 @@
 
 session_start();
 
-requireValidSession(true);
+requireValidSession();
 
 $exception = null;
 
-$userData = [];
-
-if (count($_POST) === 0 && isset($_GET['update'])) {
-    $user = User::getOne(['id' => $_GET['update']]);
-    $userData = $user->getValues();
-    $userData['password'] = null;
-} elseif(count($_POST) > 0) {
+if (count($_POST) > 0) {
     try {
-        $dbUser = new User($_POST);
-        if($dbUser->id) {
-            $dbUser->update();
-            addSuccessMsg('Usuário alterado com sucesso!');
-            header('Location: users.php');
-            exit();
-        } else {
-            $dbUser->insert();
-            addSuccessMsg('Usuário cadastrado com sucesso!');
-        }
+        $newUser = new User($_POST);
+        $newUser->insert();
+
+        addSuccessMsg('Usuário cadastrado com sucesso!');
+
         $_POST = [];
-    } catch(Exception $e) {
+    } catch (Exception $e) {
         $exception = $e;
-    } finally {
-        $userData = $_POST;
     }
 }
 
-loadTemplateView('save_user', $userData + ['exception' => $exception]);
+loadTemplateView('save_user', ['exception' => $exception]);
--- src/models/User.php
+++ src/models/User.php
@@ -16,4 +16,12 @@ class User extends Model {
     public static function getActiveUsersCount() {
         return static::getCount(['raw' => 'end_date IS NULL']);
     }
+
+    public function insert() {
+        $this->is_admin = $this->is_admin ? 1 : 0;
+        
+        if (!$this->end_date) $this->end_date = null;
+        
+        return parent::insert();
+    }
 }
