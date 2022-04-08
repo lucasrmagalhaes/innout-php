c4e2494b3193d5c894dad49653930e54f86b9360
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-08 02:29:54 -0300

    Filtros de Segurança

--- src/models/Model.php
+++ src/models/Model.php
@@ -5,15 +5,28 @@ class Model {
     protected static $columns = [];
     protected $values = [];
 
-    function __construct($arr) {
-        $this->loadFromArray($arr);
+    function __construct($arr, $sanitize = true) {
+        $this->loadFromArray($arr, $sanitize);
     }
 
-    public function loadFromArray($arr) {
+    public function loadFromArray($arr, $sanitize = true)
+    {
         if ($arr) {
-            foreach($arr as $key => $value) {
-                $this->$key = $value;
+            // $conn = Database::getConnection();
+
+            foreach ($arr as $key => $value) {
+                $cleanValue = $value;
+
+                if ($sanitize && isset($cleanValue)) {
+                    $cleanValue = strip_tags(trim($cleanValue));
+                    $cleanValue = htmlentities($cleanValue, ENT_NOQUOTES);
+                    // $cleanValue = mysqli_real_escape_string($conn, $cleanValue);
+                }
+
+                $this->$key = $cleanValue;
             }
+
+            // $conn->close();
         }
     }
 
